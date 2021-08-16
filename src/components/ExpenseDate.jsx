3dc0ed1aa4811f76b89708ca52be67ea60c42eac
Brian McConney <briancamcc@gmail.com>
2021-08-16 13:31:02 -0400

    Added ExpenseDate.css and applied classname styles

--- src/components/ExpenseDate.jsx
+++ src/components/ExpenseDate.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import "./ExpenseDate.css";
 
 function ExpenseDate(props) {
     const month = props.expenseDate.toLocaleString("en-us", {month: 'long'});
     const day = props.expenseDate.toLocaleString("en-us", {day: '2-digit'});
     const year = props.expenseDate.getFullYear();
     return (
-        <div>
-            <div> {month + " " + year + "  " + day} </div>
+        <div className ="expense-date">
+            <div className = "expense-Date__month"> {month} </div> 
+            <div className = "expense-Date__day"> {day} </div> 
+            <div className = "expense-Date__year"> {year} </div> 
         </div>
     )
 }
