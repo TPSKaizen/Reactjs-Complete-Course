c94ca13c6c8a933e510c5962a17a44e6b0987520
Brian McConney <briancamcc@gmail.com>
2021-08-16 13:54:43 -0400

    Added Expense List css

--- src/components/ExpensesList.jsx
+++ src/components/ExpensesList.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import expenses from './expenses'
 import ExpenseItem from './ExpenseItem'
+import "./ExpensesList.css"
 
 function ExpensesList() {
     return (
-        <div>
+        <div className="expenses">
             {expenses.map((item) => {
           return <ExpenseItem 
             key = {item.id}
